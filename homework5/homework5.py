2a2db061a37af97d488dde88c647ab29ccdf6c76
ilia4488 <annarichi2021@gmail.com>
2025-10-11 05:02:22 -0600

    fifth commit

---
+++ homework5/homework5.py
@@ -0,0 +1,29 @@
+print("types viyenebt rom gavigot ra tipisaa teqsti")
+num1 = "123"
+num2 = int(num1)
+print(type(num2))
+
+num = 3.7
+numint = int(num)
+numfloat = float(num)
+print(type(numint))
+print(type(numfloat))
+
+age = "25"
+
+if type(age) == str:
+    age = int(age)
+
+print(age)
+print(type(age))
+
+
+num3 = "10"
+num4 = 10
+
+print(num3 == num4)
+
+a = "5"
+a1 = int("5")
+b = 3
+print(a1 == b)
