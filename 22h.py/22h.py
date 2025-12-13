bef5c21ae1c055d7069a1e0807c26975648d3d0e
ilia4488 <annarichi2021@gmail.com>
2025-12-13 20:02:46 +0400

    second commit

---
+++ 22h.py/22h.py
@@ -0,0 +1,44 @@
+#0)
+# #sorted() alagebs sias,
+#.copy()axal sias qmnis 
+# min() umciress gamoitans,
+# max()udidess gamoitans 
+# sum()ajamebs ricxvebs
+
+#1)
+#.upper() adidebs yvela asos
+#lower apataravebs yvela asos
+#.capitalize mxolod pirvel asos datovebs dids
+#anacvlebs ert asos meoreti an ramdenime asos
+#.swapcase tu didia asoebi daapataravebs tu patarebia gaadidebs
+# #.title igivea rac capitalize ogond or sityvas aqcevs
+#.count itvlis meramdene indexszea
+
+#2)
+
+fullname = input("Enter your fullname: ").title()
+
+print(fullname)
+
+#3)
+numbers = [3, 7, 2, 9, 5]
+
+diapazoni = max(numbers) - min(numbers)
+
+print(diapazoni)
+#4)
+name = input("enter your name: ")
+
+if name[0] == name[0].upper():
+    print(name.upper())
+elif name[0] != name[0].upper():
+    print(name.capitalize())
+
+
+
+
+
+
+
+
+
