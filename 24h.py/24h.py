4a281e061f9f43af59359adbd4323addfc5479b6
ilia4488 <annarichi2021@gmail.com>
2025-12-25 14:05:58 +0400

    second commit

---
+++ 24h.py/24h.py
@@ -0,0 +1,25 @@
+#0)
+#funqcia aris kodis bloki
+#1) jer unda davwerot def da mere rame saxeli da prchxilebi romelshic sheizleba chawera cvladebis da mere magalitad ra unda daprintos 
+#2)argumenti aris mnishvneloba romelsac funqcias vawvdit gamodzaxebis dros
+
+
+#3)
+def numbers(num1, num2):
+    if num1 % num2 == 0:
+        print(num1 * num2)
+    else:
+        print(num1+num2)
+
+
+numbers(4,2)
+
+
+#4)
+def hw(width,height):
+    if width == height:
+        print(width*height)
+    else:
+        print(height+width+height+width)
+
+hw(4,2)
